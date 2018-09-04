794d5a38ac76a75aa56ef923ec680a0ccbf45b65
keyj63 <landon.key@gmail.com>
2018-09-04 06:06:46 -0500

    Add project files.

---
+++ InjectionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InjectionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Southern Company")]
+[assembly: AssemblyProduct("InjectionTests")]
+[assembly: AssemblyCopyright("Copyright © Southern Company 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24125e3d-be5b-4e57-a4c1-224438447b1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
