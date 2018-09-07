ec883154773df0c72afc245fb4feafbd8aa7e321
Landon Key <landon.key+pw@gmail.com>
2018-09-07 09:35:01 -0500

    bug fix

--- InjectionTests/Extensions/InstructionExtensions.cs
+++ InjectionTests/Extensions/InstructionExtensions.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InjectionTests.Extensions.Extensions
+namespace InjectionTests.Extensions
 {
     public static class InstructionExtensions
     {
